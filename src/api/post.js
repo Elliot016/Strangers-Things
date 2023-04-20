2afd3d16e755cd2b83075743c8c60da15e64cdc2
Edgard3033 <ed233094@outlook.com>
2023-04-20 13:11:04 -0400

    made fetch post in api

--- src/api/post.js
+++ src/api/post.js
@@ -1,13 +1,19 @@
 const COHORT_NAME = "2301-FTB-ET-WEB-AM";
 const BASE_URL = `https://strangers-things.herokuapp.com/api/${COHORT_NAME}`;
 
-export async function fetchPost() {
+export const fetchPosts = async () => {
   try {
-    const response = await fetch(`${BASE_URL}/post`);
+    const response = await fetch(`${BASE_URL}/posts`, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
     const result = await response.json();
     console.log(result);
     return result;
-  } catch (error) {
-    console.error(error);
+  } catch (err) {
+    console.error(err);
   }
-}
+};
--- src/components/ProfilePage.jsx
+++ src/components/ProfilePage.jsx
@@ -1,19 +1,9 @@
 import React, { useEffect, useState } from "react";
 import Login from "./Login";
 import { login, registerUser } from "../api/users";
-import { fetchPost } from "../api/post";
+import { fetchPosts } from "../api/post";
 
 export default function ProfilePage() {
-  const [post, setPost] = useState([]);
-
-  useEffect(() => {
-    async function getPost() {
-      const result = await fetchPost();
-      setPost(result);
-    }
-    getPost();
-  }, []);
-
   return (
     <div>
       <h1>Welcome "username"</h1>
