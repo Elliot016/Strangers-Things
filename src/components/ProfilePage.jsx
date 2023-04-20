e312a68067da0b409efc5573b396dc3f802d9aa6
Edgard3033 <ed233094@outlook.com>
2023-04-20 12:29:41 -0400

    post apierror showing up

--- src/components/ProfilePage.jsx
+++ src/components/ProfilePage.jsx
@@ -1,8 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Login from "./Login";
 import { login, registerUser } from "../api/users";
+import { fetchPost } from "../api/post";
 
 export default function ProfilePage() {
+  const [post, setPost] = useState([]);
+
+  useEffect(() => {
+    async function getPost() {
+      const result = await fetchPost();
+      setPost(result);
+    }
+    getPost();
+  }, []);
+
   return (
     <div>
       <h1>Welcome "username"</h1>
